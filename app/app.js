b61b0b4d835d4f2b3dfc2599020c4ee18d6062e4
Rella <c11740@gdcan.com>
2018-08-03 15:11:43 -0400

    Express Application

--- app/app.js
+++ app/app.js
@@ -1,5 +1,20 @@
-var http = require('http');
+var express = require('express');
 
+
+var app = express();
+
+//main root of application, execute function literal. Can do something with req and res
+app.get('/', function(req, res){
+
+    //dont need mime types
+    res.send('<h1>Roux Academy Meetups </h1>');
+});
+
+var server = app.listen(3000, function(){
+    console.log('port 3000');
+})
+
+/*
 var myServer = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type" : "text/html"});
   response.write('<h1>Roux Meetups</h1>');
@@ -8,3 +23,4 @@ var myServer = http.createServer(function(request, response) {
 
 myServer.listen(3000);
 console.log('Go to http://localhost:3000 on your browser');
+*/
