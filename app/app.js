8b70e67e0b7b1cf0080fb61b2a8aa487af53281c
Rella <c11740@gdcan.com>
2018-08-08 11:11:00 -0400

    Completed basic routing

--- app/app.js
+++ app/app.js
@@ -5,20 +5,39 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
-  var info = '';
-  dataFile.speakers.forEach(function(item) {
-    info += `
-    <li>
-      <h2>${item.name}</h2>
-      <p>${item.summary}</p>
-    </li>
-    `;
-  });
+
   res.send(`
-      <h1>Roux Academy Meetups</h1>
-      ${info}
+     <h1>Welcome</h1>
+     <p>Roux Academy Meetups put artists together from all walks of life</p>
   `);
 });
+
+app.get('/speakers', function(req, res) {
+    var info = '';
+    dataFile.speakers.forEach(function(item) {
+      info += `
+      <li>
+        <h2>${item.name}</h2>
+        <p>${item.summary}</p>
+      </li>
+      `;
+    });
+    res.send(`
+        <h1>Roux Academy Meetups</h1>
+        ${info}
+    `);
+  }); 
+// the "/:" allows you to pass in variable to the route
+  app.get('/speakers/:speakerid', function(req, res) {
+  
+    var speaker = dataFile.speakers[req.params.speakerid];
+    res.send(`
+        <h1>${speaker.title}</h1>
+        <h2>with ${speaker.name}</h2>
+        <p>${speaker.summary}</p>
+       
+    `);
+  }); 
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
