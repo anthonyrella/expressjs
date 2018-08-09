8f8a0603f55a0ebb0f2f61c23f6d84cff438a3af
Rella <c11740@gdcan.com>
2018-08-09 13:51:25 -0400

    Passing data to template through render and global

--- app/app.js
+++ app/app.js
@@ -8,6 +8,9 @@ app.set('appData', dataFile);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
+//this app variable will now be applicable to any of the views 
+app.locals.siteTitle = 'Roux Meetups';
+
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
