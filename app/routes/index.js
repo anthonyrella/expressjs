6dfc19714b16a27988fc107f86ed12d6fca48964
Rella <c11740@gdcan.com>
2018-08-09 14:27:41 -0400

    Conditionals and Loops through ejs

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+
+  data.speakers.forEach(function(item){
+    pagePhotos = pagePhotos.concat(item.artwork);
+  })
+
   res.render('index', {
+    
+    artwork: pagePhotos,
     pageTitle: 'Home',
     pageID: 'home'
   });
