a193070fffb27a97b568ea06814f8abefc7528d8
Rella <c11740@gdcan.com>
2018-08-10 14:28:06 -0400

    Completed handling post requests

--- app/public/js/feedback.js
+++ app/public/js/feedback.js
@@ -1,6 +1,19 @@
 $(function() {
   $.getJSON('api', updateFeedback);
 
+  $('.feedback-form').submit(function(e){
+
+    //need to prevent form from reloading server!!! This is a temporary fix so you can keeping adding to replies before refresh
+    e.preventDefault();
+    //jquery post send some data to url . updateFeedback as third parameter to run after post to update the page 
+    $.post('api', {
+      name: $('#feedback-form-name').val(), 
+      title: $('#feedback-form-title').val(), 
+      message: $('#feedback-form-message').val()
+    }, updateFeedback);
+
+  });
+
   function updateFeedback(data) {
    var output = '';
    $.each(data,function(key, item) {
@@ -8,7 +21,7 @@ $(function() {
      output += '       <div class="feedback-item media">';
      output += '         <div class="feedback-info media-body">';
      output += '           <div class="feedback-head">';
-     output += '             <div class="feedback-title">' + item.title + '<small class="feedback-name label label-info"></small></div>';
+     output += '             <div class="feedback-title">' + item.title + ' <small class="feedback-name label label-info"> '+ item.name +'</small></div>';
      output += '           </div>';
      output += '           <div class="feedback-message">' + item.message + '</div>';
      output += '         </div>';
